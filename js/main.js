8044e74780eb596bf3be655a68f2b24c767550cd
M4lf0r <arielmalfor@live.com>
2020-11-11 18:32:28 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-var nome = "Ariel Alves";
-var idade = 22;
-var idade2 = 10;
-var frase = "japão é o melhor time do mundo";
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+//var lista = ["Maçã", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join("-"));
+//alert(lista[1]);
+
+
+
+
+
+//var nome = "Ariel Alves";
+//var idade = 22;
+//var idade2 = 10;
+//var frase = "japão é o melhor time do mundo";
 //alert(nome+" tem "+idade);
-console.log(nome);
-console.log(idade+idade2);
-console.log(frase.replace("japão", "Brasil"));
-console.log(frase.toUpperCase());
-alert(frase.replace("japão", "Brasil"));
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.replace("japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//alert(frase.replace("japão", "Brasil"));
