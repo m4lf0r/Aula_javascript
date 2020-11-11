fad499acd6cbd4856f5f39649eeaa3815fe9562f
M4lf0r <arielmalfor@live.com>
2020-11-11 19:12:55 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaidade(idade){  //validar idade
+    var validar;
+    if(idade >= 18){
+        validar = true
+    }else(
+        validar = false
+    )
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaidade(idade));
+
+//alert(soma(5, 10));
 
 /*
 var dt = new Date(); //data
