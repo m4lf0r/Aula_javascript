3fc4942f641fcb7990055124793411f73f1d4049
M4lf0r <arielmalfor@live.com>
2020-11-11 18:02:56 -0200

    Comm1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Ariel Alves";
+var idade = 22;
+var idade2 = 10;
+var frase = "japão é o melhor time do mundo";
+//alert(nome+" tem "+idade);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("japão", "Brasil"));
+console.log(frase.toUpperCase());
+alert(frase.replace("japão", "Brasil"));
