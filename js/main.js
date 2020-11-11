0ddb44fdf4e1df17cec357e4d90312e698ad0656
M4lf0r <arielmalfor@live.com>
2020-11-11 20:21:57 -0200

    comm3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>"; //Aparecer text na tela
+    //console.log(document.getElementById("agradecimento")) //aparecer no console
+    //alert("Obrigado por clicar");
+}
 
+function redirecionar(){
+    window.open("https://github.com/m4lf0r/Aula_javascript/"); //redirecionamento de page(nova aba)
+    //window.location.href = "https://github.com/m4lf0r/Aula_javascript/"; //redirecionamento na mesma page
+}
 
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+ // alert("Trocar texto"); 
+}
 
+function voltar(elemento){
+   // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
 
+function load(){
+    alert("página carregada");
+}
 
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
 /*
 function soma(n1, n2){
     return n1 + n2;
